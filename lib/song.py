1a8f6bd8c6ae559dd3a7cf9770549be2f6b28a2a
Will Olson <will.olson.b@gmail.com>
2024-11-23 12:04:51 -0600

    Passing tests.

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,34 @@
 class Song:
-    pass
+    count = 0
+    genre_count = {}
+    artist_count = {}
+    genres = set()
+    artists = set()
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+
+        Song.count += 1
+
+        if genre in Song.genre_count:
+            Song.genre_count[genre] += 1
+        else:
+            Song.genre_count[genre] = 1
+
+        if artist in Song.artist_count:
+            Song.artist_count[artist] += 1
+        else:
+            Song.artist_count[artist] = 1
+
+        Song.genres.add(genre)
+        Song.artists.add(artist)
+
+    @classmethod
+    def get_genres(cls):
+        return list(cls.genres)
+
+    @classmethod
+    def get_artists(cls):
+        return list(cls.artists)
